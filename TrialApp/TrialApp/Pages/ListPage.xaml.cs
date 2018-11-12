01a4464753645c4d7c938c78677612a363ba2559
Prem Rajah <prajah@ctlondon.com>
2018-11-12 16:23:34 +0000

    finished grouped lists, starting handling

--- TrialApp/TrialApp/Pages/ListPage.xaml.cs
+++ TrialApp/TrialApp/Pages/ListPage.xaml.cs
@@ -16,11 +16,21 @@ namespace TrialApp.Pages
 		{
 			InitializeComponent ();
 
-            myList.ItemsSource = new List<Models.Contact>
+            myList.ItemsSource = new List<Models.ContactGroup>
             {
-                   new Models.Contact(){Name = "Prem Rajah", Status = "Let's talk", ImageUrl ="https://placeimg.com/100/100/people"},
-                   new Models.Contact(){Name = "Mike Jackson", Status = "Time to party", ImageUrl ="https://placeimg.com/100/100/people"},
-                   new Models.Contact(){Name = "Patrick Paul", Status = "Zoom Zoom Zoom", ImageUrl ="https://placeimg.com/100/100/people"}
+                new Models.ContactGroup("P", "P")
+                {
+                    new Models.Contact(){Name = "Prem Rajah", Status = "Let's talk", ImageUrl ="https://placeimg.com/100/100/people"}
+                },
+                new Models.ContactGroup("M", "M")
+                {
+                    new Models.Contact(){Name = "Mike Jackson", Status = "Time to party", ImageUrl ="https://placeimg.com/100/100/people"}
+                },
+                new Models.ContactGroup("C", "C")
+                {
+                     new Models.Contact(){Name = "Clark Kent", Status = "Zoom Zoom Zoom", ImageUrl ="https://placeimg.com/100/100/people"}
+                }
+                  
             };
 		}
 	}
